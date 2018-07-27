aa782833c31e2876a6ced2c1d33f7f600acff244
k-hench <kosmas.hench@gmx.de>
2018-07-27 18:29:36 +0200

    initialize hypogen package

---
+++ R/hypogen_annoplot.R
@@ -0,0 +1,116 @@
+#' Loading the annotation dataframe
+#'
+#' \code{hypo_annotation_get} loads the genome annotation.
+#'
+#' Hypogen comes with the annotation data of the halmet genome.
+#' To load the data into R \code{hypo_annotation_get} makes use of
+#' the \strong{rtracklayer} package.
+#' The function returns a list containg two data frames: the first one
+#' holds the general mRNA extends, the second ond the detailed Exon boundaries.
+#'
+#' Only the data of a specified range on a specific LG is loaded.
+#' Genes of interes as well as genes of minor interest can be specified and
+#' the number of lines that the mRNAs can be specified to avoid overlapping
+#' when plotting the annotations.
+#'
+#' @param searchLG string skalar (mandatory), should be on of "LG01" - "LG24"
+#' @param xrange integer vector (mandatory), data range to be loaded(start bp - end bp).
+#'   Positions are defined with respect to the LG, NOT the overall genomic position.
+#' @param genes_of_interest string vector (optional), tags specific gene names for highlighting when plotting.
+#'   (needs to exactly match the gene name of the original gff file)
+#' @param genes_of_sec_interest string vector (optional), tags specific gene names for secondary highlighting
+#'  when plotting. (needs to exactly match the gene name of the original gff file)
+#' @param anno_rown integer skalar (optional), defines the number of gene rows to avoid overlapping.
+#'
+#' @seealso \code{\link{hypo_annotation_baseplot}}
+hypo_annotation_get <- function(searchLG,xrange,genes_of_interest=c(),
+                                    genes_of_sec_interest=c(),anno_rown=3){
+  gfffile <- system.file("extdata", "HP.annotation.named.gff", package="hypogen")
+  gff_filter <- list(seqid=searchLG)
+  data <- as.data.frame(readGFF(gfffile,filter=gff_filter)) %>%
+    mutate(Parent=as.character(Parent))
+
+  mRNAs <- data %>% filter(type=='mRNA',end>xrange[1],start<xrange[2]) %>%
+    ungroup() %>%  mutate(yl=row_number()%%anno_rown+2) %>% rowwise()%>%
+    mutate(checkStart =ifelse(start<xrange[1],-Inf,start),
+           checkEnd =ifelse(end>xrange[2],Inf,end),
+           ps=ifelse(strand=='-',checkEnd,checkStart),
+           pe=ifelse(strand=='-',checkStart,checkEnd),
+           labelx=mean(c(sort(c(xrange[1],ps))[2],
+                         sort(c(xrange[2],pe))[1])),
+           window='bold(Gene)',
+           clr=ifelse(Parentgenename %in% genes_of_interest,"zzz3",
+                      ifelse(Parentgenename %in% genes_of_sec_interest,"zzz2","zzz1")),
+           label = unlist(strsplit(tolower(Parentgenename), "_"))[1]) %>%
+    select(seqid, type, start, end, strand, Name, ID,
+           oId, Parentgene, Parentgenename,  yl, checkStart, checkEnd,
+          ps, pe, labelx, window, clr,label);
+
+
+
+  names(mRNAs)[names(mRNAs)=='ID'] <- 'Parent'
+
+  exons <- data %>% filter(type=='exon',end>xrange[1],start<xrange[2]) %>%
+    merge(.,mRNAs %>% select(Parent,yl,clr),by='Parent',all.x=T) %>%
+    mutate(ps=ifelse(strand=='-',end,start),
+           pe=ifelse(strand=='-',start,end),
+           window='bold(Gene)') %>%
+    select(Parent, seqid, type, start, end,  strand,
+           Name, ID, oId, Parentgene, Parentgenename, yl, clr, ps, pe, window)
+
+
+  return(list(mRNAs,exons))}
+
+#' Plotting the annotation dataframe
+#'
+#' \code{hypo_annotation_baseplot} initalizes the annotation plot.
+#'
+#' This is wrapper that includes loding the annotations, and initalizing the ggplot.
+#' Individual data tracks should be realized using facetting over the "window" column.
+#'
+#' @param searchLG string skalar (mandatory), should be on of "LG01" - "LG24"
+#' @param xrange integer vector (mandatory), data range to be loaded(start bp - end bp).
+#'   Positions are defined with respect to the LG, NOT the overall genomic position.
+#' @param genes_of_interest string vector (optional), tags specific gene names for highlighting when plotting.
+#'   (needs to exactly match the gene name of the original gff file)
+#' @param genes_of_sec_interest string vector (optional), tags specific gene names for secondary highlighting
+#'  when plotting. (needs to exactly match the gene name of the original gff file)
+#' @param anno_rown integer skalar (optional), defines the number of gene rows to avoid overlapping.
+#' @param width float skalar (0-1, optional), defines the height of the exon boxes.
+#'
+#' @examples
+#'
+#' hypo_annotation_baseplot(searchLG = "LG03", xrange = c(0,100000),
+#'   genes_of_interest = 'SUN1_4') +
+#'   geom_line(data = dataframe,
+#'             aes(x = x, y = y, color = window)) +
+#'   facet_grid(window~.,scales='free_y',
+#'              switch = 'y',labeller = label_parsed,
+#'              as.table = TRUE)
+#'
+#' @seealso \code{\link{hypo_annotation_get}}, \code{\link{theme_hypo_anno_extra}}
+hypo_annotation_baseplot <- function(..., searchLG, xrange, genes_of_interest = c(),
+                                 genes_of_sec_interest = c(), anno_rown = 3,
+                                 width = .1){
+
+  df_list <- hypo_annotation_get(searchLG = searchLG,xrange = xrange,
+                                 genes_of_interest = genes_of_interest,
+                                 genes_of_sec_interest = genes_of_sec_interest,
+                                 anno_rown = anno_rown)
+
+  ggplot()+
+  geom_rect(data=df_list[[2]],
+            aes(xmin = ps, xmax = pe, ymin = yl-(width/2), ymax = yl+(width/2),
+                fill = clr, col = clr, group = Parent),
+            lwd = .1)+
+  geom_segment(data = (df_list[[1]] %>% filter(strand %in% c('+', '-'))),
+               aes(x = ps, xend = pe, y = yl, yend = yl, group = Parent),
+               lwd=.2,arrow = arrow(length = unit(2,"pt"),type = 'closed'),
+               color = 'black')+
+  geom_segment(data = (df_list[[1]] %>% filter(!strand %in% c('+', '-'))),
+               aes(x = ps, xend = pe, y = yl, yend = yl, group = Parent),
+               lwd = .2, color = 'black')+
+  geom_text(data = df_list[[1]],
+            aes(x = labelx, label = gsub('000000', '...', label),y = yl-.5))
+  }
+
---
+++ R/hypogen_basics.R
@@ -0,0 +1,138 @@
+#' Shaded LG backgrounds
+#'
+#' \code{geom_hypo_LG} adds the linkage group (LG) boundaries to background of a ggplot.
+#'
+#' To add context to genome wide plots of a statistic (e.g. Fst) this function
+#' adds a alternating shaded background to the plot.
+#'
+#'
+#' @examples
+#'
+#' @seealso \code{\link{scale_fill_hypo_LG_bg}}
+#'
+#' ggplot() +
+#'   geom_hypo_LG()+
+#'   scale_color_hypo_LG()
+geom_hypo_LG <- function(...){
+  geom_rect(inherit.aes = F,data=hypo_karyotype,
+            aes(xmin = GSTART,
+                xmax = GEND,
+                ymin = -Inf,
+                ymax = Inf,
+                fill = GROUP))
+}
+
+#' Format genome wide x axis
+#'
+#' \code{scale_x_hypo_LG} adjusts the x axis to match the hamet genome.
+#'
+#' Each of the LGs is labelled with on tick which is placed in the
+#' center of the LG.
+#' The should be combined with \code{hypogen::geom_hypo_LG}
+#'
+#' @seealso \code{\link{geom_hypo_LG}}, \code{\link{scale_color_hypo_LG}}
+#'
+#' @examples
+#'
+#' ggplot() +
+#'   geom_hypo_LG() +
+#'   scale_x_hypo_LG()
+scale_x_hypo_LG <- function(..., name = '', expand = c(0,0),
+                            breaks = (hypo_karyotype$GSTART + hypo_karyotype$GEND) / 2,
+                            labels = 1:24, position = "top"){
+  scale_x_continuous(name = name, expand = expand,breaks = breaks,
+                     labels = labels,position = position)
+}
+
+#' Set LG color palette
+#'
+#' \code{scale_color_hypo_LG} adjusts the color scale with default color for each LG.
+#'
+#' Each of the LGs is assigned to one default color.
+#'
+#' \strong{! CAREFUL:} should not be used if additional color levels exiest.
+#'
+#' @seealso \code{\link{geom_hypo_LG}}, \code{\link{scale_x_hypo_LG}}
+scale_color_hypo_LG <- function(..., name = 'LG'){
+  scale_color_manual(...,name = name, values = hypo_clr_LGs)
+}
+
+#' Set LG fill
+#'
+#' \code{scale_fill_hypo_LG_bg} sets the color scheme for the LG background shading.
+#'
+#' Fill values are set for tnhe two LG classes (even/odd LG nr).
+#'
+#' \strong{! CAREFUL:} LG fill values are set to "zz1" $ zz2.
+#' If additional fill levels exist, the fill values of the LGs should be appended
+#' to the desired fill scale.
+#'
+#' @seealso \code{\link{geom_hypo_LG}}, \code{\link{scale_x_hypo_LG}}, \code{\link{scale_color_hypo_LG}}
+scale_fill_hypo_LG_bg <- function(){
+  scale_fill_manual(values = c(NA, hypo_clr_lg), guide = F)
+}
+
+#' Set hypogen plot theme
+#'
+#' \code{theme_hypo} sets the default hypogen plot theme.
+#'
+#' This theme is optimized for genome wide plots.
+#'
+#' @seealso \code{\link{theme_hypo_anno_extra}}
+theme_hypo <- function (...) {
+  theme_bw(base_size = 10, base_family = "xkcd") %+replace%
+    theme(...,
+          plot.background = element_blank(),
+          panel.background  = element_blank(),
+          panel.grid=element_blank(),
+          panel.border = element_blank(),
+          axis.title.x = element_blank(),
+          axis.line = element_line(),
+          strip.background = element_rect(fill = NA, color = hypo_clr_lg),
+          legend.background = element_rect(fill = "transparent", color = NA),
+          legend.key = element_rect(fill = "transparent", color = NA)
+    )
+}
+
+#' Set hypogen plot theme
+#'
+#' \code{theme_hypo_anno_extra} adjusts the theme for annotation plots.
+#'
+#' @seealso \code{\link{theme_hypo}}
+theme_hypo_anno_extra <- function(...){
+  theme(axis.title.x = element_text(),
+        panel.grid.major.x = element_line(color = hypo_clr_lg),
+        panel.grid.minor.x = element_line(color = hypo_clr_lg),
+        axis.title.y = element_blank(),
+        strip.placement = 'outside')
+}
+
+
+#' Determining the LG from the genomic position
+#'
+#' \code{hypo_which_CHROM_s} determines the LG from the genomic position.
+#'
+#' The function finds the respective LG on which a specific genomic position (1 - 559,649,677)
+#' resides.
+#'
+#' @param x interger skalar (mandatory), a bp position on the hamlet genome.
+#'
+#' @seealso \code{\link{hypo_which_CHROM}}
+hypo_which_CHROM_s <- function(x){
+  CHROM <- hypo_karyotype$CHROM[x >= hypo_karyotype$GSTART & x <= hypo_karyotype$GEND]
+  CHROM
+}
+
+#' Determining the LG from the genomic positions
+#'
+#' \code{hypo_which_CHROM} determines the LG from a vector of genomic position.
+#'
+#' The function vectorizes the \code{hypo_which_CHROM_s} function.
+#'
+#' @param x interger vector (mandatory), a vector of bp positions on the hamlet genome.
+#'
+#' @seealso \code{\link{hypo_which_CHROM}}
+hypo_which_CHROM <- function(x){
+  CHROM <- map(x,hypo_which_CHROM_s)
+  unlist(CHROM)
+}
