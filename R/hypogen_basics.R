71ea47af41c55afb29ea97e33127d198ed12c6d9
khench@geomar.de <khench@geomar.de>
2019-03-03 20:25:58 +0100

    color

--- R/hypogen_basics.R
+++ R/hypogen_basics.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  cat("--- Welcome to", crayon::blue(hypogen), "---")
+  cat("--- Welcome to", crayon::blue("hypogen"), "---")
 }
 #' Shaded LG backgrounds
 #'
